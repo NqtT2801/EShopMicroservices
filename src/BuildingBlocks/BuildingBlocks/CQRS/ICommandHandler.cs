bc69c2aa021c051f68f0c533850e1e7e7228addc
NqtT2801 <nguyenanhquan2801@gmail.com>
2024-09-08 21:37:02 +0700

    initial

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommandHandler<in TCommand> : ICommand<Unit>
+        where TCommand : ICommand<Unit>
+    { };
+
+    public interface ICommandHandler<in TCommand, TResponse> 
+        : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+        where TResponse : notnull
+    { }
+}
